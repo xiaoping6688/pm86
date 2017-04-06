fe58dd913a1d678f38627457ee89d6f73c5e230a
Eric <github@ericjj.com>
2017-04-06 17:48:54 +0800

    Update ws.js

--- frontend/views/ws.js
+++ frontend/views/ws.js
@@ -164,7 +164,7 @@ export default (__this) => {
   };
 
   socket.onopen = function () {
-    connection.send('Ping'); // Send the message 'Ping' to the server
+    console.log('Ping'); // Send the message 'Ping' to the server
     setTimeout(ask, 1000);
   };
 
